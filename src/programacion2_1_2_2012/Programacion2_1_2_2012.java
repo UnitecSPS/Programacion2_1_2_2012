ceed0b1106002d9dc5e93cb8d212e4472b680942
Carlos Gochez <carlos.gochez@unitec.edu>
2012-04-13 08:43:41 -0600

    mi primer cambio

---
+++ src/programacion2_1_2_2012/Programacion2_1_2_2012.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion2_1_2_2012;
+
+/**
+ *
+ * @author Gotcha
+ */
+public class Programacion2_1_2_2012 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
