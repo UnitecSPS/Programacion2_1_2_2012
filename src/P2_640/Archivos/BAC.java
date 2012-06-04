0a797bfce628c87ae37c0340289773753c83c23b
Carlos Gochez <cgochez@lawgical.com>
2012-06-04 17:51:22 -0600

    acciones hechas en el tg2

--- src/P2_640/Archivos/BAC.java
+++ src/P2_640/Archivos/BAC.java
@@ -4,12 +4,54 @@
  */
 package P2_640.Archivos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author CarlosArmando
  */
 public class BAC {
+    
     public static void main(String args[]){
         JBank bank =new JBank();
+        int op=0;
+        Scanner lea = new Scanner(System.in);
+        
+        do{
+            try{
+                System.out.println("\n1-Agregar Cuenta");
+                System.out.println("2-Listar Cuentas");
+                System.out.println("3-Buscar Cuenta");
+                System.out.println("4- Salir");
+                System.out.print("\nEscoja Opcion: ");
+                op = lea.nextInt();
+                
+                switch( op ){
+                    case 1:
+                        System.out.println("Ingrese Nombre: ");
+                        bank.addCuenta(lea.next());
+                        break;
+                    case 2:
+                        System.out.println("Listar Activas (s) o Inactivas (n)?: ");
+                        char resp = lea.next().charAt(0);
+                        bank.listar( resp == 's' ? true : false );
+                        break;
+                    case 3:
+                        System.out.println("Ingrese Codigo a buscar: ");
+                        if( bank.buscar(lea.nextInt()) )
+                            System.out.println("La Cuenta Existe");
+                        else
+                            System.out.println("La Cuenta NO existe");
+                        break;
+                        
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println("Error: " + e.getMessage());
+            }   
+            
+        }while( op != 4);
+        
     }
 }
--- src/P2_640/Archivos/JBank.java
+++ src/P2_640/Archivos/JBank.java
@@ -7,6 +7,7 @@ package P2_640.Archivos;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Date;
 
 /**
  *
@@ -37,4 +38,70 @@ public class JBank {
         rCod.close();
         
     }
+    
+    private int getCodigo( )throws IOException{
+        RandomAccessFile rCod = new RandomAccessFile("codigos.bac","rw");
+        
+        int cod = rCod.readInt(); //leo el codigo disponible
+        rCod.seek(0); //me regreso de nuevo
+        rCod.writeInt(cod + 1);//actualizo el archivo
+        rCod.close();
+        
+        return cod;
+    }
+    
+    public void addCuenta( String cliente )throws IOException{
+        //me voy al final
+        rCuentas.seek( rCuentas.length() );
+        //escribo el codigo
+        rCuentas.writeInt( getCodigo() );
+        //nombre
+        rCuentas.writeUTF(cliente);
+        //saldo
+        rCuentas.writeDouble(500);
+        //fecha ultima
+        rCuentas.writeLong( new Date().getTime() );
+        //activo
+        rCuentas.writeBoolean(true);
+    }
+    
+    public void listar(boolean activas)throws IOException{
+        //desde el inicio
+        rCuentas.seek(0);
+        
+        while( rCuentas.getFilePointer() < rCuentas.length() ){
+            int cod = rCuentas.readInt();
+            String client = rCuentas.readUTF();
+            double sal = rCuentas.readDouble();
+            long fechault = rCuentas.readLong();
+            
+            if( rCuentas.readBoolean() == activas ){
+                System.out.println(cod + "-" + client + " Lps " + sal +
+                        " ultima vez usada: " + new Date(fechault) );
+            }
+        }
+    }
+    
+    public boolean buscar(int codcuenta)throws IOException{
+        //desde el inicio
+        rCuentas.seek(0);
+        while( rCuentas.getFilePointer() < rCuentas.length() ){
+            //guardo la pos antes de comenzar a leer el registro
+            long pos = rCuentas.getFilePointer();
+            
+            int cod = rCuentas.readInt();
+            rCuentas.readUTF();
+            //me salto el saldo + fecha + activo = 17 bytes
+            rCuentas.seek( rCuentas.getFilePointer() + 17);
+            
+            if( cod == codcuenta ){
+                rCuentas.seek(pos);
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
+    
 }
